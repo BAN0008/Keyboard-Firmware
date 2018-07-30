8538fa8545547939f47dc5c066f166991dcf8fe7
BAN0008 <BAN0008@tc.vic.edu.au>
2018-07-30 18:54:27 +1000

    Update debug.h

--- debug.h
+++ debug.h
@@ -4,27 +4,68 @@
 #include <stdio.h>
 #include <stdlib.h>
 #include <stdint.h>
+#include <unistd.h>
 #include <stdbool.h>
+#include <stddef.h>
 #include <string.h>
 
 #define PIN_COUNT 24
 
+//Define Pin States
 #define OUTPUT 0
 #define INPUT 1
 #define INPUT_PULLUP 2
 #define HIGH 1
 #define LOW 0
 
+//Define keycodes
+#define KEY_LEFT_CTRL   0x80
+#define KEY_LEFT_SHIFT  0x81
+#define KEY_LEFT_ALT    0x82
+#define KEY_LEFT_GUI    0x83
+#define KEY_RIGHT_CTRL  0x84
+#define KEY_RIGHT_SHIFT 0x85
+#define KEY_RIGHT_ALT   0x86
+#define KEY_RIGHT_GUI   0x87
+#define KEY_UP_ARROW    0xDA
+#define KEY_DOWN_ARROW  0xD9
+#define KEY_LEFT_ARROW  0xD8
+#define KEY_RIGHT_ARROW 0xD7
+#define KEY_BACKSPACE   0xB2
+#define KEY_TAB         0xB3
+#define KEY_RETURN      0xB0
+#define KEY_ESC         0xB1
+#define KEY_INSERT      0xD1
+#define KEY_DELETE      0xD4
+#define KEY_PAGE_UP     0xD3
+#define KEY_PAGE_DOWN   0xD6
+#define KEY_HOME        0xD2
+#define KEY_END         0xD5
+#define KEY_CAPS_LOCK   0xC1
+#define KEY_F1          0xC2
+#define KEY_F2          0xC3
+#define KEY_F3          0xC4
+#define KEY_F4          0xC5
+#define KEY_F5          0xC6
+#define KEY_F6          0xC7
+#define KEY_F7          0xC8
+#define KEY_F8          0xC9
+#define KEY_F9          0xCA
+#define KEY_F10         0xCB
+#define KEY_F11         0xCC
+#define KEY_F12         0xCD
+
 struct virtual_pin
 {
 	uint8_t mode;
-	bool state;
+	bool    state;
 };
 
 struct virtual_pin virtual_pins[PIN_COUNT];
-bool virtual_matrix[MATRIX_ROWS][MATRIX_COLUMNS] = {false};
+bool   virtual_matrix[MATRIX_ROWS][MATRIX_COLUMNS] = {false};
 
 bool keyboard_began = false;
+bool serial_began   = false;
 
 void initialize_virtual_pins()
 {
@@ -57,6 +98,11 @@ void simulate_matrix()
 			}
 		}
 	}
+}
+
+void delay(int milliseconds)
+{
+	usleep(milliseconds * 1000);
 }
 
 void pinMode(uint8_t pin, uint8_t mode)
@@ -119,13 +165,18 @@ void Keyboard_begin()
 	keyboard_began = true;
 	printf("\x1b[32mINFO: \x1b[34m`Keyboard.begin()`\x1b[32m called\x1b[0m\n");
 }
+void Keyboard_end()
+{
+	keyboard_began = false;
+	printf("\x1b[32mINFO: \x1b[34m`Keyboard.end()`\x1b[32m called\x1b[0m\n");
+}
 void Keyboard_press(uint8_t keycode)
 {
 	if (!keyboard_began)
 	{
 		printf("\x1b[1;33mWARNING: \x1b[34m`Keyboard.press()`\x1b[1;33m called before \x1b[34m`Keyboard.begin()`\x1b[0m\n");
 	}
-	printf("\x1b[35mPressing key \x1b[34m0x%x\x1b[0m\n", keycode);
+	printf("\x1b[35mPressing key \x1b[34m0x%x/%c\x1b[0m\n", keycode, keycode);
 }
 void Keyboard_release(uint8_t keycode)
 {
@@ -133,7 +184,82 @@ void Keyboard_release(uint8_t keycode)
 	{
 		printf("\x1b[1;33mWARNING: \x1b[34m`Keyboard.release()`\x1b[1;33m called before \x1b[34m`Keyboard.begin()`\x1b[0m\n");
 	}
-	printf("\x1b[35mReleasing key \x1b[34m0x%x\x1b[0m\n", keycode);
+	printf("\x1b[35mReleasing key \x1b[34m0x%x/%c\x1b[0m\n", keycode, keycode);
+}
+
+void Serial_begin(uint32_t speed)
+{
+	serial_began = true;
+	printf("\x1b[32mINFO: \x1b[34m`Serial.begin(%u)`\x1b[1;32m called\x1b[0m\n", speed);
+}
+void Serial_end()
+{
+	serial_began = false;
+	printf("\x1b[32mINFO: \x1b[34m`Serial.end()`\x1b[1;32m called\x1b[0m\n");
+}
+size_t Serial_println(char *val)
+{
+	if (!serial_began)
+	{
+		printf("\x1b[1;31mERROR: \x1b[34m`Serial.println()`\x1b[1;33m called before \x1b[34m`Serial.begin()`\x1b[0m\n");
+		return 0;
+	}
+	else
+	{
+		printf("\x1b[35mSerial print \x1b[34m`%s`\x1b[0m\n", val);
+		return strlen(val);
+	}
+}
+uint8_t Serial_read()
+{
+	if (!serial_began)
+	{
+		printf("\x1b[1;31mERROR: \x1b[34m`Serial.read()`\x1b[1;33m called before \x1b[34m`Serial.begin()`\x1b[0m\n");
+		return 0x0;
+	}
+	else
+	{
+		uint8_t return_val;
+		printf("\x1b[35mSerial read \x1b[34m");
+		scanf(" %hhu", &return_val);
+		printf("\x1b[0m");
+		return return_val;
+	}
+}
+char *Serial_readStringUntil(char terminator)
+{
+	char *return_val;
+	if (!serial_began)
+	{
+		printf("\x1b[1;31mERROR: \x1b[34m`Serial.readStringUntil()`\x1b[1;33m called before \x1b[34m`Serial.begin()`\x1b[0m\n");
+		return_val = "";
+	}
+	else
+	{
+		printf("\x1b[1;33mWARNING: input ends with newline regardless of what's passed to this function\x1b[0m\n");
+		printf("\x1b[35mSerial readStringUntil \x1b[34m");
+		scanf(" %s", return_val);
+		printf("\x1b[0m");
+	}
+	return return_val;
+}
+size_t Serial_write(uint8_t bytes[])
+{
+	if (!serial_began)
+	{
+		printf("\x1b[1;31mERROR: \x1b[34m`Serial.write()`\x1b[1;33m called before \x1b[34m`Serial.begin()`\x1b[0m\n");
+		return 0;
+	}
+	else
+	{
+		printf("\x1b[35mSerial write \x1b[34m`0x");
+		for (uint32_t i = 0; i < sizeof(bytes); i++)
+		{
+			printf("%X", bytes[i]);
+		}
+		printf("`\x1b[0m\n");
+		return sizeof(bytes);
+	}
 }
 
 void debug_prompt()
@@ -215,8 +341,20 @@ void debug_prompt()
 const struct
 {
 	void (*begin)(void);
+	void (*end)(void);
 	void (*press)(uint8_t keycode);
 	void (*release)(uint8_t keycode);
-} Keyboard = {&Keyboard_begin, &Keyboard_press, &Keyboard_release};
+} Keyboard = {&Keyboard_begin, &Keyboard_end, &Keyboard_press, &Keyboard_release};
+
+const struct
+{
+	void    (*begin)(uint32_t speed);
+	void    (*end)(void);
+	size_t  (*print)(char *val);
+	size_t  (*println)(char *val);
+	uint8_t (*read)(void);
+	char *  (*readStringUntil)(char terminator);
+	size_t  (*write)(uint8_t bytes[]);
+} Serial = {&Serial_begin, &Serial_end, &Serial_println, &Serial_println, &Serial_read, &Serial_readStringUntil, &Serial_write};
 
 #endif //_DEBUG_H
